933be1b862c0e4c02a3682646b627c48789e0ac3
Oluwagbenga Bello <bellogbenga43@gmail.com>
2025-07-04 17:46:32 +0100

    updated the middleware

--- services/mealkit-service/src/middleware/mealkitMiddleware.ts
+++ services/mealkit-service/src/middleware/mealkitMiddleware.ts
@@ -11,5 +11,18 @@ export const validatMealkit = async (
       "string.empty": "name cannot be empty",
       "any.requred": "name is required",
     }),
+    ingredients: joi.string().required().messages({
+      "string.empty": "ingredients cannot be empty",
+      "any.required": "ingredients is required",
+    }),
+    instructions: joi.string().required().messages({
+      "string.empty": "instructions cannot be empty",
+      "any.required": "instructions is required",
+    }),
+    price: joi.number().required().messages({
+      "string.empty": "price cannot be empty",
+      "any.required": "price is required",
+      "number.base": "invalid type, please provide a valid number",
+    }),
   });
 };
